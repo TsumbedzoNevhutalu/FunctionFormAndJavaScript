4c398cdf99fe122382667f2891cd31a19cd238c0
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-04-12 16:15:17 +0200

    commit the index.html javascript and the css file

---
+++ script.js
@@ -0,0 +1,10 @@
+function getFormValues(){
+    let fname =document.getElementsByName("fname").value;
+    let lname =document.getElementsByName("lname").value;
+    let email= document.getElementsByName("email").value;
+    
+    console.log(lname);
+    console.log(fname);
+    console.log(email);
+    
+}
